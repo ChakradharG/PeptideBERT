572b0a23fff4ee895f517d383715e24031007572
CG_Lab <gchakradhar72@gmail.com>
2023-07-20 17:16:39 -0400

    all datasets

--- data/download_data.py
+++ data/download_data.py
@@ -3,13 +3,37 @@ import urllib.request
 
 def download_hemolysis():
     urllib.request.urlretrieve(
+        'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/hemo-positive.npz',
+        './data/hemo-positive.npz',
+    )
+    urllib.request.urlretrieve(
+        'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/hemo-negative.npz',
+        './data/hemo-negative.npz',
+    )
+
+
+def download_solubility():
+    urllib.request.urlretrieve(
+        'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/soluble.npz',
+        './data/sol-positive.npz',
+    )
+    urllib.request.urlretrieve(
+        'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/insoluble.npz',
+        './data/sol-negative.npz',
+    )
+
+
+def download_nonfouling():
+    urllib.request.urlretrieve(
         'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/human-positive.npz',
-        './data/positive.npz',
+        './data/nf-positive.npz',
     )
     urllib.request.urlretrieve(
         'https://github.com/ur-whitelab/peptide-dashboard/raw/master/ml/data/human-negative.npz',
-        './data/negative.npz',
+        './data/nf-negative.npz',
     )
 
 
 download_hemolysis()
+download_solubility()
+download_nonfouling()
